0c2022de924be579273500dfaa66b8a4c4c3672b
Shashikanth004 <kanthshashi755@gmail.com>
2026-05-28 14:00:02 +0530

    Update api.js to use VITE_API_BASE_URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || '/api'
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL,
   timeout: 60000,
   headers: { 'Content-Type': 'application/json' }
 })
+
 
 // Intercept requests to add auth token
 api.interceptors.request.use(config => {
